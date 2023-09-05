2c3fbab61126c07b6dec6f3b5f5b35faa35baa47
Levente Varga <varga.levente@happyempire.hu>
2023-09-05 14:50:17 +0200

    Testing Spring REST Mappings

--- src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/controller/DemoController.java
+++ src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/controller/DemoController.java
@@ -1,11 +1,48 @@
 package com.unideb.sfm.Szoftverfejlesztesmernokoknek.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.unideb.sfm.Szoftverfejlesztesmernokoknek.model.Demo;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class DemoController {
+
+    private Map<String, Demo> demoList = new HashMap<>(){{
+       put("0", new Demo("1", "John Doe", "25", "Budapest", "1995-01-01"));
+       put("1", new Demo("2", "Jane Doe", "23", "Debrecen", "1997-01-01"));
+       put("2", new Demo("3", "Jack Doe", "21", "Szeged", "1999-01-01"));
+    }};
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/people")
+    public String getPeople(){
+        String people = "";
+        for (Demo demo : demoList.values()) {
+            people += demo.toString();
+        }
+        return people;
+    }
+
+    @GetMapping("/person/{id}")
+    public String getPerson(@PathVariable int id){
+        //If id is out of bounds, throw 404
+        if(id < 0 || id >= demoList.size()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Person not found");
+        }
+        return demoList.get(id).toString();
+    }
+
+    @PostMapping("/person")
+    public String addPerson(@RequestBody Demo demo){
+        demoList.put(UUID.randomUUID().toString(), demo);
+        return demo.toString();
+    }
 
 }
--- src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/model/Demo.java
+++ src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/model/Demo.java
@@ -1,5 +1,70 @@
 package com.unideb.sfm.Szoftverfejlesztesmernokoknek.model;
 
-public class Demo {
+import java.util.Date;
 
+public class Demo {
+    String id;
+    String name;
+    String age;
+    String birthPlace;
+    String birthDate;
+
+    public Demo(String id, String name, String age, String birthPlace, String birthDate) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.birthPlace = birthPlace;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "<b>{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", birthPlace='" + birthPlace + '\'' +
+                ", birthDate=" + birthDate +
+                "}</b><br>";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
 }
