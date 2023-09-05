b47d0e44db1f92fc083a26885f617b3abda842dc
Levente Varga <varga.levente@happyempire.hu>
2023-09-05 11:13:16 +0200

    Base Spring with dependencies

---
+++ src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/SzoftverfejlesztesMernokoknekApplication.java
@@ -0,0 +1,13 @@
+package com.unideb.sfm.Szoftverfejlesztesmernokoknek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzoftverfejlesztesMernokoknekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzoftverfejlesztesMernokoknekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/SzoftverfejlesztesMernokoknekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unideb.sfm.Szoftverfejlesztesmernokoknek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzoftverfejlesztesMernokoknekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
