b09189223bc8a396a689bd1a88f2b05b11d05562
Levente Varga <varga.levente@happyempire.hu>
2023-09-13 16:26:53 +0200

    Movie model bővítve, getAll, getById, getByCategory, getPopular lekérések működnek.

--- src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/model/Movie.java
+++ src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/model/Movie.java
@@ -18,16 +18,33 @@ public class Movie {
             generator = "movie_sequence"
     )
     private Integer id;
+
+    //@NotNull(message = "Title cannot be null")
     private String title;
+
+    //@NotNull(message = "Title cannot be null")
     @Column(name = "release_year")
     private Integer year;
+
+    //@NotNull(message = "Title cannot be null")
     private double rating;
 
-    public Movie(Integer id, String title, Integer year, double rating) {
+    @Column(name="overview", length=2048)
+    private String overview;
+
+    private String poster_path;
+
+    private String categories;
+
+//Constructors
+    public Movie(Integer id, String title, Integer year, double rating, String overview, String poster_path, String categories) {
         setId(id);
         setTitle(title);
         setYear(year);
         setRating(rating);
+        setOverview(overview);
+        setPoster_path(poster_path);
+        setCategories(categories);
     }
 
     public Movie() {} // default empty constructor
@@ -49,22 +66,26 @@ public class Movie {
                 ", title='" + title + "'" +
                 ", year=" + year +
                 ", rating=" + rating +
+                ", overview=" + overview +
+                ", poster_path=" + poster_path +
+                ", category=" + categories +
                 '}';
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Movie movie)) return false;
-        return Objects.equals(getId(), movie.getId()) && Objects.equals(getTitle(), movie.getTitle()) && Objects.equals(getYear(), movie.getYear()) && Objects.equals(getRating(), movie.getRating());
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return Double.compare(movie.rating, rating) == 0 && Objects.equals(id, movie.id) && Objects.equals(title, movie.title) && Objects.equals(year, movie.year) && Objects.equals(overview, movie.overview) && Objects.equals(poster_path, movie.poster_path) && Objects.equals(categories, movie.categories);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getTitle(), getYear(), getRating());
+        return Objects.hash(id, title, year, rating, overview, poster_path, categories);
     }
 
-// getters and setters
+// getters and setters and other add and remove methods
     public Integer getId() {
         return id;
     }
@@ -95,5 +116,37 @@ public class Movie {
 
     public void setRating(double rating) {
         this.rating = rating;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void addCategory(String category) {
+        this.categories += category+";";
+    }
+
+    public void removeCategory(String category) {
+        this.categories = this.categories.replace(category+";", "");
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
     }
 }
