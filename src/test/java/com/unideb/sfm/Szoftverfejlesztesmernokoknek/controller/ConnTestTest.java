336d7422a8bdb76382c6dba67837f38645b9f136
Levente Varga <varga.levente@happyempire.hu>
2023-09-23 15:23:43 +0200

    Docker FIX

--- src/test/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/controller/ConnTestTest.java
+++ src/test/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/controller/ConnTestTest.java
@@ -1,8 +1,13 @@
 package com.unideb.sfm.Szoftverfejlesztesmernokoknek.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ConnTestTest {
 
     @Test
