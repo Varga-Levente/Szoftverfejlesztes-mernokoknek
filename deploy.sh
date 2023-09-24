a8c2e2ef2af7f8ad9c2949953b281553c02c2493
Varga Levente István <47919682+Varga-Levente@users.noreply.github.com>
2023-09-24 15:04:34 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ start=$(date +"%s")
 ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
 docker pull vlevente2001/sfm:latest
 
-CONTAINER_NAME=bmiapp
+CONTAINER_NAME=sfmapp
 if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
     if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
         echo "Container is running -> stopping it..."
