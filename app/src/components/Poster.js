a0a615a76d8968c70d7f8d01a6c8948314621e38
deles30 <danika30001@gmail.com>
2023-10-03 15:29:04 +0200

    added scrollable posters fix

--- app/src/components/Poster.js
+++ app/src/components/Poster.js
@@ -4,49 +4,49 @@ import './Poster.css';
 const Poster = () => {
     return (
         <div className="d-flex flex-nowrap overflow-x-auto scroll-div">
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">Indiana Jones and the Dial of Destiny</p>
             </div>
-            <div className="poster p-2"><img className="movie-poster" src="movie.jpg"/>
+            <div className="poster p-2"><img alt="poster" className="movie-poster" src="movie.jpg"/>
                 <p className="movie-title">END</p>
             </div>
         </div>
