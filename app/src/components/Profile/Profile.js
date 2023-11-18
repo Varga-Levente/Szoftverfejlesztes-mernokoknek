84c7aa9392cba4d58fcda36494ce7fa0e3e2465a
kalma <secondaddress27@gmail.com>
2023-11-18 23:34:33 +0100

    cinema selector v1

--- app/src/components/Profile/Profile.js
+++ app/src/components/Profile/Profile.js
@@ -2,7 +2,27 @@ import React from 'react';
 import './Profile.css';
 const Profile = () => {
     return (
-        <h1 className="profile">Profile</h1>
+        <div className={"profile_page"}>
+            <h1 className="profile">Profile</h1>
+            <div className={"profile_panel"}>
+                <div className={"left_side"}>
+                    <img src={"/avatar.jpg"} className={"avatar_v1"} alt={"avatar"}/>
+                </div>
+                <div className={"right_side"}>
+                    <div className={"my_tickets"}>
+                        <div className={"ticket"}>
+                            <h2>My Tickets</h2>
+                        </div>
+                    </div>
+                    <div className={"my_credit"}>
+
+                    </div>
+                    <div className={"history"}>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
