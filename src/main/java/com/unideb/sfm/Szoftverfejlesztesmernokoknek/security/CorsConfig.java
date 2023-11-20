d66a6e896c12c40140664bd0d41d7e980133ccd1
Varga Levente István <varga.levente@happyempire.hu>
2023-11-20 19:11:50 +0100

    Try fixing cors issue with disabling cors.

---
+++ src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/security/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.unideb.sfm.Szoftverfejlesztesmernokoknek.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Ez engedélyezi az összes eredetet, ezt finomíthatod szükség szerint
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Origin", "Accept", "X-Requested-With"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
