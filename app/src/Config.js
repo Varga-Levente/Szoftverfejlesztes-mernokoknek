9064bdaa7de43f49ee05360673d5d2249163ef25
Varga Levente István <varga.levente@happyempire.hu>
2023-11-21 23:18:06 +0100

    Changed settings to prod

--- app/src/Config.js
+++ app/src/Config.js
@@ -1,3 +1,3 @@
-//export const API_URL = "http://vps-df7347a7.vps.ovh.net:8080/api/v1";
-export const API_URL = "http://localhost:8080/api/v1";
+export const API_URL = "http://vps-df7347a7.vps.ovh.net:8080/api/v1";
+//export const API_URL = "http://localhost:8080/api/v1";
 export const APP_NAME = "CinemApp Pro+";
