ee6d1d379334ab43070ce8eef7f8e67cc6417602
Varga Levente István <varga.levente@happyempire.hu>
2023-11-22 22:58:59 +0100

    asda

--- app/src/components/VerticalMenu.js
+++ app/src/components/VerticalMenu.js
@@ -11,59 +11,59 @@ const VerticalMenu = () => {
 
         // Az összes a li elemen belüli elem színének beállítása
         elementsInsideListItem.forEach((element) => {
-          element.style.color = '#F74346'; // Változtasd meg a színt az igényeid szerint
-        });
-      };
-
-      // Eseményfigyelő a listitem hover hatásának megszűnésekor
-      const handleListItemLeave = (event) => {
-        // Az összes a li elemen belüli elem színének visszaállítása eredeti színre
-        event.currentTarget.querySelectorAll('*').forEach((element) => {
-          element.style.color = ''; // Üres string a szín visszaállításához
+            element.style.color = '#F74346'; // Változtasd meg a színt az igényeid szerint
         });
     };
 
-  return (
-    <ul className="nav flex-column">
-        <li
-            className="nav-item listitem active"
-            onMouseEnter={handleListItemHover}
-            onMouseLeave={handleListItemLeave}
-        >
-            <a className="nav-link active navigationlink" href="/#">
-                <i className="material-icons mui">movie</i><br />
-                <span className="menuname">Billboard</span>
-            </a>
-        </li>
-        <li className="nav-item listitem"
-            onMouseEnter={handleListItemHover}
-            onMouseLeave={handleListItemLeave}
-        >
-            <a className="nav-link navigationlink" href="/coming-soon">
-                <i className="material-icons mui">movie_filter</i><br />
-                <span className="menuname">Coming soon</span>
-            </a>
-        </li>
-        <li className="nav-item listitem"
-            onMouseEnter={handleListItemHover}
-            onMouseLeave={handleListItemLeave}
-        >
-            <a className="nav-link navigationlink" href="/food">
-                <FontAwesomeIcon icon={faHamburger} className="fas faicon" /><br />
-                <span className="menuname">Food</span>
-            </a>
-        </li>
-        <li className="nav-item listitem" style={{ paddingBottom: 15 }}
-            onMouseEnter={handleListItemHover}
-            onMouseLeave={handleListItemLeave}
-        >
-            <a className="nav-link navigationlink" href="/profile">
-                <FontAwesomeIcon icon={faUser} className="fas faicon" /><br />
-                <span className="menuname">Profile</span>
-            </a>
-        </li>
-    </ul>
-  );
+    // Eseményfigyelő a listitem hover hatásának megszűnésekor
+    const handleListItemLeave = (event) => {
+        // Az összes a li elemen belüli elem színének visszaállítása eredeti színre
+        event.currentTarget.querySelectorAll('*').forEach((element) => {
+            element.style.color = ''; // Üres string a szín visszaállításához
+        });
+    };
+
+    return (
+        <ul className="nav flex-column">
+            <li
+                className="nav-item listitem active"
+                onMouseEnter={handleListItemHover}
+                onMouseLeave={handleListItemLeave}
+            >
+                <a className="nav-link active navigationlink" href="/#">
+                    <i className="material-icons mui">movie</i><br />
+                    <span className="menuname">Billboard</span>
+                </a>
+            </li>
+            <li className="nav-item listitem"
+                onMouseEnter={handleListItemHover}
+                onMouseLeave={handleListItemLeave}
+            >
+                <a className="nav-link navigationlink" href="/coming-soon">
+                    <i className="material-icons mui">movie_filter</i><br />
+                    <span className="menuname">Coming soon</span>
+                </a>
+            </li>
+            <li className="nav-item listitem"
+                onMouseEnter={handleListItemHover}
+                onMouseLeave={handleListItemLeave}
+            >
+                <a className="nav-link navigationlink" href="/food">
+                    <FontAwesomeIcon icon={faHamburger} className="fas faicon" /><br />
+                    <span className="menuname">Food</span>
+                </a>
+            </li>
+            <li className="nav-item listitem" style={{ paddingBottom: 15 }}
+                onMouseEnter={handleListItemHover}
+                onMouseLeave={handleListItemLeave}
+            >
+                <a className="nav-link navigationlink" href="/profile">
+                    <FontAwesomeIcon icon={faUser} className="fas faicon" /><br />
+                    <span className="menuname">Profile</span>
+                </a>
+            </li>
+        </ul>
+    );
 }
 
 export default VerticalMenu;
