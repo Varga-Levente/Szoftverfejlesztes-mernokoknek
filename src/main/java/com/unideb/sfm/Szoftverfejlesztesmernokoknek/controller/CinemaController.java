d893b8d8cbc64010d7bc55fcc34d3f81e30cdabe
Sum Fuk <56839243+sumfuk0@users.noreply.github.com>
2023-11-25 10:30:50 +0100

    CinemaService & CinemaController

--- src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/controller/CinemaController.java
+++ src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/controller/CinemaController.java
@@ -4,11 +4,12 @@ import com.unideb.sfm.Szoftverfejlesztesmernokoknek.model.Cinema;
 import com.unideb.sfm.Szoftverfejlesztesmernokoknek.repository.CinemaRepository;
 import com.unideb.sfm.Szoftverfejlesztesmernokoknek.service.CinemaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/api/v1/cinema")
 public class CinemaController {
 
     @Autowired
@@ -16,5 +17,19 @@ public class CinemaController {
     @Autowired
     private CinemaService cinemaService;
 
-    // ;(
+    @DeleteMapping("remove/{cinemaId}")
+    public ResponseEntity<?> removeCinemaById(@PathVariable("cinemaId") Integer cinemaId) {
+        return cinemaService.removeCinemaById(cinemaId);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<?> addCinema(@RequestBody Cinema cinema) {
+        return cinemaService.addCinema(cinema);
+    }
+
+    @GetMapping("getAll")
+    public Iterable<Cinema> getAll() {
+        return cinemaService.getAll();
+    }
+
 }
--- src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/service/CinemaService.java
+++ src/main/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/service/CinemaService.java
@@ -3,7 +3,10 @@ package com.unideb.sfm.Szoftverfejlesztesmernokoknek.service;
 import com.unideb.sfm.Szoftverfejlesztesmernokoknek.model.Cinema;
 import com.unideb.sfm.Szoftverfejlesztesmernokoknek.repository.CinemaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CinemaService {
@@ -11,5 +14,22 @@ public class CinemaService {
     @Autowired
     private CinemaRepository cinemaRepository;
 
-    //????????
+    public ResponseEntity<?> addCinema(Cinema cinema) {
+        cinemaRepository.save(cinema);
+        return ResponseEntity.ok("Cinema added successfully");
+    }
+
+    public ResponseEntity<?> removeCinemaById(Integer cinemaId) {
+        Cinema cinema = cinemaRepository.findById(cinemaId).orElse(null);
+        if (cinema == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        cinemaRepository.deleteById(cinemaId);
+        return ResponseEntity.ok("Cinema deleted successfully");
+    }
+
+    public List<Cinema> getAll() {
+        return cinemaRepository.findAll();
+    }
 }
