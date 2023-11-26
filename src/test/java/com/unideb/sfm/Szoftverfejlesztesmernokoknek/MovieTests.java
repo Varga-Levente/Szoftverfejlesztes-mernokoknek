ff8609e8ebf3d3d8c0ec542bd2987edc29547e05
NemesLevente18 <120102549+NemesLevente18@users.noreply.github.com>
2023-11-26 13:21:42 +0100

    Add files via upload

--- src/test/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/MovieTests.java
+++ src/test/java/com/unideb/sfm/Szoftverfejlesztesmernokoknek/MovieTests.java
@@ -8,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -53,50 +54,5 @@ public class MovieTests {
         assertEquals("Test poster path", movie.getPoster_path());
         assertEquals("Test trailer id", movie.getYt_trailer_id());
         assertEquals(2021, movie.getYear());
-    }
-
-    Movie movie_1 = new Movie() {
-        {
-            setTitle("Bambi");
-            setCategories("Action");
-            setRating(7);
-            setOverview("Too much blood");
-            setPoster_path("Test poster path 1");
-            setYt_trailer_id("Test trailer id 1");
-            setYear(2021);
-        }
-    };
-    Movie movie_2 = new Movie() {
-        {
-            setTitle("Fast and Furious");
-            setCategories("Drama");
-            setRating(6);
-            setOverview("A bit too long");
-            setPoster_path("Test poster path 2");
-            setYt_trailer_id("Test trailer id 2");
-            setYear(2021);
-        }
-    };
-    Movie movie_3 = new Movie() {
-        {
-            setTitle("Alien Isolation");
-            setCategories("Sci-fi");
-            setRating(8);
-            setOverview("Very good and interesting");
-            setPoster_path("Test poster path 3");
-            setYt_trailer_id("Test trailer id 3");
-            setYear(2021);
-        }
-    };
-
-    public void getAllMovies_Succes() throws Exception {
-        List<Movie> movies = new ArrayList<>(Arrays.asList(movie_1, movie_2, movie_3));
-        Mockito.when(movierepository.findAll()).thenReturn(movies);
-        mockMvc.perform(MockMvcRequestBuilders.get("../../../../../../main/resources/").contentType("data.json"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(3)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].title", Matchers.is("Bambi")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].title", Matchers.is("Fast and Furious")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[2].title", Matchers.is("Alien Isolation")));
     }
 }
